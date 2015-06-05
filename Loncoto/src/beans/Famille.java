711591851d9b2ed555e5b3976fee045220c2e3ac
Emmanuel Berroir <e.berroir@gmail.com>
2015-06-05 16:02:41 +0200

    Début ajout de la classe Famille

---
+++ Loncoto/src/beans/Famille.java
@@ -0,0 +1,28 @@
+package beans;
+
+public class Famille {
+	private int id;
+	private String nom;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Famille(int id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
+	public Famille() {
+		this(0, "");
+	}
+	
+	
+}
