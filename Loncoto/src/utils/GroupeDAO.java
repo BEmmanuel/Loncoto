9180de0e90fc845b78be8e51091039ba14e7eed9
hunter686 <yang.hunter686@gmail.com>
2015-06-08 12:27:25 +0200

    commit de groupeDAO

--- Loncoto/src/utils/GroupeDAO.java
+++ Loncoto/src/utils/GroupeDAO.java
@@ -2,26 +2,54 @@ package utils;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import beans.Groupe;
 
 public class GroupeDAO implements IGroupeDAO {
+	
+	private EntityManager em;
+	
+	
+	public EntityManager getEm() {
+		return em;
+	}
+	
+	@PersistenceContext
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
 
 	@Override
+	@Transactional
 	public List<Groupe> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("from Groups",Groupe.class).getResultList();
 	}
 
 	@Override
-	public List<Groupe> findByID(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public Groupe findByID(int id) {
+		return em.find(Groupe.class, id);
 	}
 
 	@Override
+	@Transactional
 	public Groupe save(Groupe groupe) {
-		// TODO Auto-generated method stub
-		return null;
+		if (groupe == null)
+			return groupe;
+	
+		if (groupe.getId() == 0)
+		{
+			em.persist(groupe);
+		}
+		else
+		{
+			groupe = em.merge(groupe);
+		}
+		return groupe;
 	}
 
 }
--- Loncoto/src/utils/IGroupeDAO.java
+++ Loncoto/src/utils/IGroupeDAO.java
@@ -7,6 +7,6 @@ import beans.Groupe;
 public interface IGroupeDAO {
 
 	public List<Groupe> findAll();
-	public List<Groupe> findByID(int id);
+	public Groupe findByID(int id);
 	public Groupe save(Groupe groupe);
 }
