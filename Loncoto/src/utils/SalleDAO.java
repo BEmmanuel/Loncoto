61a4d25495f0e623e4ecfc80169f859049813ae8
Krevet <Krevet@192.168.3.6>
2015-06-08 11:53:08 +0200

    creation DAO Materiel, Salle, Site

---
+++ Loncoto/src/utils/SalleDAO.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.util.List;
+
+import beans.Salle;
+
+public class SalleDAO implements ISalleDAO {
+
+	@Override
+	public List<Salle> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Salle findByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Salle save(Salle salle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Loncoto/src/utils/SiteDAO.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.util.List;
+
+import beans.Site;
+
+public class SiteDAO implements ISiteDAO {
+
+	@Override
+	public List<Site> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Site findByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Site save(Site site) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
