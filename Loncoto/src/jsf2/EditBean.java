3ce1ba2b155586d73e8ff6ea5782e3159ef7af5e
Krevet <Krevet@192.168.3.6>
2015-06-08 10:51:15 +0200

    Ajout des Beans JSF2

---
+++ Loncoto/src/jsf2/EditBean.java
@@ -0,0 +1,20 @@
+package jsf2;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.springframework.stereotype.Component;
+
+import com.sun.faces.facelets.tag.jstl.core.SetHandler;
+
+
+
+
+@Component
+@ManagedBean
+public class EditBean {
+	
+}
---
+++ Loncoto/src/jsf2/ListeBean.java
@@ -0,0 +1,18 @@
+package jsf2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ManagedBean
+public class ListeBean {
+	
+	
+
+}
