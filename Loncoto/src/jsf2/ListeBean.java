8051ed74881b6dfe2ef34b4f6025c6907f4f6677
Emmanuel Berroir <e.berroir@gmail.com>
2015-06-08 15:25:27 +0200

    Début ListeBean, pour les interventions

--- Loncoto/src/jsf2/ListeBean.java
+++ Loncoto/src/jsf2/ListeBean.java
@@ -8,10 +8,25 @@ import javax.faces.event.AjaxBehaviorEvent;
 
 import org.springframework.stereotype.Component;
 
+import beans.Intervention;
+import utils.IInterventionDAO;
+
 
 @Component
 @ManagedBean
 public class ListeBean {
+	
+	private IInterventionDAO interventionDAO;
+	private List<Intervention> interventons;
+	
+	
+	public IInterventionDAO getInterventionDAO() {
+		return interventionDAO;
+	}
+
+	public void setInterventionDAO(IInterventionDAO interventionDAO) {
+		this.interventionDAO = interventionDAO;
+	}
 	
 	
 
