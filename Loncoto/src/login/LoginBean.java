95ed6249cc04e9187641513fd8fe68a6702ab2a1
Emmanuel Berroir <e.berroir@gmail.com>
2015-06-11 14:44:34 +0200

    ajout de MaterielRest

--- Loncoto/src/login/LoginBean.java
+++ Loncoto/src/login/LoginBean.java
@@ -4,18 +4,14 @@ import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.tribes.group.GroupChannel.InterceptorIterator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import beans.Administrateur;
-import beans.Intervenant;
 import beans.Utilisateur;
 import utils.IIntervenantDAO;
 
